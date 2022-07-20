0eb84bbc46d6a9cb2b4fd064b0429810d00c8ad7
LaVon <leonidmedica@gmail.com>
2022-07-20 12:57:44 +0200

    add little test

---
+++ src/store/tasks.ts
@@ -0,0 +1,23 @@
+export const sum =(salary:number,n:number)=> salary + n;
+export const sub =(salary:number,n:number)=> salary - n;
+export const div =(salary:number,n:number)=> salary /n;
+export const mult =(salary:number,n:number)=> salary * n;
+
+export type ActionType ={
+    type:"SUM" |"TEST" |"SUB"|"DIV"|"MULT",
+        n:number
+}
+export const salaryReducer = (state:number,action:ActionType)=>{
+    switch (action.type){
+        case "SUM":
+            return state+action.n
+        case "SUB":
+            return state-action.n
+        case "DIV":
+            return state/action.n
+        case "MULT":
+            return state*action.n
+        default:
+            return state
+    }
+}
