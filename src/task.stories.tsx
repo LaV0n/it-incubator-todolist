a26c1d52756e2d677fa7fd1134c5c0d2c54b9d7c
LaVon <leonidmedica@gmail.com>
2022-08-10 12:52:07 +0200

    tl12

---
+++ src/task.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {action} from "@storybook/addon-actions";
+import { Task } from './task';
+
+export default {
+    title: 'Todolist/Task',
+    component: Task,
+} as ComponentMeta<typeof Task>;
+
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+const baseArgs = {
+    changeTaskStatus: action("Status changed inside Task"),
+    changeTaskTitle: action("Title changed inside Task"),
+    removeTask: action("Remove button inside Task was clicked")
+}
+
+export const TaskIsDoneExample = Template.bind({});
+TaskIsDoneExample.args = {
+    ...baseArgs,
+   task:{id:'1',isDone:true, title:"JS"},
+   // todolistId:"todoListId1"
+};
+
+export const TaskIsNotDoneExample = Template.bind({});
+TaskIsNotDoneExample.args = {
+    ...baseArgs,
+    task:{id:'1',isDone:false, title:"JS"},
+    //todolistId:"todoListId1"
+};
+
+
